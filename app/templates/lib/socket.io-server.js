d11c9affbfebd3dc43cb57a43c1c007c023e4711
Yosuke Furukawa <furukawa.yosuke@dena.jp>
2013-07-08 10:32:28 +0900

    Remove socket.io transport require

--- app/templates/lib/socket.io-server.js
+++ app/templates/lib/socket.io-server.js
@@ -1,10 +1,10 @@
 module.exports = function(server){
   <% if (use_heroku) { %>
   var transports = ["xhr-polling"];
-  <% } else {%>
-  var transports = require('socket.io/lib/manager').defaultTransports;
-  <% } %>
   var io = require('socket.io').listen(server, { transports: transports});
+  <% } else {%>
+  var io = require('socket.io').listen(server);
+  <% } %>
 
   io.sockets.on('connection', function(socket) {
     socket.on('comment', function(data) {
